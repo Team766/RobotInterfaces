046534e43ad4cf601b8c72bdaa1748ad4c69507a
Blevenson <blevenson68@gmail.com>
2016-07-07 18:36:13 -0700

    Finished adding basic interfaces

---
+++ src/interfaces/DigitalInputReader.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public interface DigitalInputReader {
+	/**
+	 * Get the value from a digital input channel. Retrieve the value of a
+	 * single digital input channel from the FPGA.
+	 *
+	 * @return the status of the digital input
+	 */
+	public boolean get();
+}
