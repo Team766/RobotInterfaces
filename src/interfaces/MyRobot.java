901736bc88874a601e21d5750c2aa418a12c3e3c
Blevenson <blevenson68@gmail.com>
2016-07-13 02:01:18 -0700

    Creates overarching interface for robot code

---
+++ src/interfaces/MyRobot.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+public interface MyRobot {
+	public void robotInit();
+	public void disabledInit();
+	public void autonomousInit();
+	public void teleopInit();
+	public void testInit();
+	
+	public void disabledPeriodic();
+	public void autonomousPeriodic();
+	public void teleopPeriodic();
+	public void testPeriodic();
+}
