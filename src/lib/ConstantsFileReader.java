cfb11ec9d3a0a8d43fe8d9a379f758a4c3a4f43c
Blevenson <blevenson68@gmail.com>
2017-01-03 23:10:54 -0800

    Class that allows access to constants saved in file.
    Used for quickly updating values: similar to SmartDashboard

---
+++ src/lib/ConstantsFileReader.java
@@ -0,0 +1,77 @@
+package lib;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+
+/**
+ * Class for loading in constants from the Constants file.
+ * Constants that need to be tuned / changed
+ * Returns zero if constant not found.
+ * 
+ * Data is read from CSV files in the format:
+ * 
+ * kDriveDistance, 100
+ * kPDrive,1
+ * kIDrive,0
+ * 
+ * @author Brett Levenson
+ *
+ */
+
+public class ConstantsFileReader {
+	
+	private static ConstantsFileReader instance;
+	
+	public static String fileName = "";
+	public static boolean onRobot = true;
+	
+	private BufferedReader reader;
+	private HashMap<String, Double> constants;
+	
+	public static ConstantsFileReader getInstance(){
+		if(instance == null)
+			instance = new ConstantsFileReader();
+		return instance;
+	}
+	
+	public double get(String key){
+		return constants.containsKey(key) ? constants.get(key) : 0.0;
+	}
+	
+	public ConstantsFileReader(){
+		onRobot = fileName.equals("constants.csv") || !fileName.equals("simConstants.csv");
+		
+		System.out.println("Loading constants file: " + fileName);
+		constants = new HashMap<String, Double>();
+		
+		loadConstants();
+	}
+	
+	public void loadConstants(){
+		String currLine = "";
+		try {
+			if(!onRobot)
+				reader = new BufferedReader(new FileReader(this.getClass().getClassLoader().getResource(fileName).getPath()));
+			else
+				reader = new BufferedReader(new FileReader(fileName));
+			
+			currLine = reader.readLine();
+			String[] tokens;
+			while(currLine != null){
+				//Separate the key from the value
+				tokens = currLine.split(",");
+				
+				constants.put(tokens[0], Double.valueOf(tokens[1]));
+				
+				currLine = reader.readLine();
+			}
+			reader.close();
+
+		} catch (Exception e) {
+			System.err.println("Failed to load constants file!\t:(");
+			e.printStackTrace();
+		}
+	}
+	
+}
