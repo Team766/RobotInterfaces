b2823119fcc3437ae24921e112ee0b13704ac54d
blevenson <blevenson68@gmail.com>
2017-01-25 18:49:59 -0800

    Digital input reader functionalliy implemented

--- src/interfaces/RobotProvider.java
+++ src/interfaces/RobotProvider.java
@@ -12,11 +12,14 @@ public abstract class RobotProvider {
 	protected GyroReader[] gyros = new GyroReader[13];
 	protected HashMap<String, CameraReader> cams = new HashMap<String, CameraReader>();
 	protected JoystickReader[] joysticks = new JoystickReader[3];
+	protected DigitalInputReader[] digInputs = new DigitalInputReader[5];
 	
 	//HAL
 	public abstract SpeedController getMotor(int index);
 	
 	public abstract EncoderReader getEncoder(int index1, int index2);
+	
+	public abstract DigitalInputReader getDigitialInputer(int index);
 	
 	public abstract SolenoidController getSolenoid(int index);
 	
