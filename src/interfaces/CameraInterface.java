35bf2ff6b8f32334737dd9ca1bae10eccec1ee2c
blevenson <blevenson68@gmail.com>
2017-02-20 18:04:07 -0800

    CameraInterfaces allows USB camera to send Mats

--- src/interfaces/CameraInterface.java
+++ src/interfaces/CameraInterface.java
@@ -5,4 +5,5 @@ import org.opencv.core.Mat;
 public interface CameraInterface {	
 	public void startAutomaticCapture(VidSourceInterface camera);
 	public void getFrame(Mat img);
+	public void putFrame(Mat img);
 }
