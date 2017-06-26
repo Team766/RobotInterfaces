143cebdad5d6a753e49d98d07b08af8bb3aa5dc9
qxzcode <qxzcraft@gmail.com>
2017-06-25 17:39:23 -0700

    Implement parsing messages from the dashboard

--- src/lib/Dashboard.java
+++ src/lib/Dashboard.java
@@ -4,13 +4,22 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 import static lib.LogMessage.Level;
 
 /**
- * This actor handles all interfacing with the custom dashboard.
+ * This actor handles all interfacing with the custom dashboard. It opens a TCP
+ * server on port {@value PORT}, and accepts a single connection. (If that connection
+ * is closed/lost, a new one will be accepted.) {@link DashboardMessage}s are
+ * generated when messages are received from the dashboard. Various methods are
+ * also provided for sending messages to the dashboard.
  * <p>
- * See <a href="https://github.com/Team766/FRC-Dashboard">Team766/FRC-Dashboard</a>
+ * This class is a singleton. Access its instance via {@link #getInstance()
+ * Dashboard.getInstance()}.
+ * <p>
+ * See <a href="https://github.com/Team766/FRC-Dashboard">Team766/FRC-Dashboard on GitHub</a>
  *
  * @author Quinn Tucker
  */
@@ -39,8 +48,13 @@ public class Dashboard extends Actor {
 	
 	private ServerSocket serverSocket;
 	private Socket socket;
-	private Reader in;
 	private Writer out;
+	private Reader in;
+	
+	private StringBuilder inBuf;
+	private String msgType;
+	private int msgLen = -1;
+	private List<String> msgArgs;
 	
 	@Override
 	public void init() {
@@ -53,8 +67,8 @@ public class Dashboard extends Actor {
 	
 	@Override
 	public void run() {
-		StringBuilder inBuf = null;
 		while (enabled) {
+			// accept a connection if not already connected
 			try {
 				if (socket == null || socket.isClosed()) {
 					socket = serverSocket.accept();
@@ -67,9 +81,11 @@ public class Dashboard extends Actor {
 				sleep();
 				continue;
 			}
+			
+			// read a character from the socket
 			try {
 				int ch = in.read();
-				if (ch == -1 || ch == ':') {
+				if (ch == -1 || ch == ':') { // end of a token (EoF or colon)
 					processToken(inBuf.toString());
 					inBuf.setLength(0); // clear the buffer
 				} else {
@@ -82,7 +98,26 @@ public class Dashboard extends Actor {
 	}
 	
 	private void processToken(String token) {
-		// TODO
+		if (msgType == null) {
+			msgType = token;
+		} else if (msgLen == -1) {
+			try {
+				msgLen = Integer.parseInt(token);
+				if (msgLen < 0) throw new NumberFormatException();
+				msgArgs = new ArrayList<>();
+			} catch (NumberFormatException e) {
+				log(Level.ERROR, "Dashboard sent bad argument count: \""+token+"\"");
+			}
+		} else {
+			msgArgs.add(token);
+			msgLen--;
+		}
+		if (msgLen == 0) {
+			sendMessage(new DashboardMessage(msgType, msgArgs));
+			msgType = null;
+			msgLen = -1;
+			msgArgs = null;
+		}
 	}
 	
 	@Override
