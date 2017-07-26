8d1684330ff577fe69459e8d964ff8fc28abef9d
qxzcode <qxzcraft@gmail.com>
2017-07-26 13:44:17 -0700

    Move common functionality into Controller superclass

--- src/lib/control/Controller.java
+++ src/lib/control/Controller.java
@@ -7,9 +7,61 @@ public abstract class Controller {
 	
 	protected double output;
 	
-	protected boolean done = false;
+	protected double dtMin;
+	protected double dt, pos, vel, acc;
+	private double lastUpdateTime;
 	
-	public abstract double update(double currentPos);
+	protected boolean isFirstUpdate = true;
+	protected boolean done = false;
+	protected boolean reversed = false;
+	
+	protected Controller() {
+		this(0.0);
+	}
+	
+	protected Controller(double dtMin) {
+		this.dtMin = dtMin;
+	}
+	
+	protected double doFirstUpdate() {
+		return doUpdate();
+	}
+	
+	protected abstract double doUpdate();
+	
+	public double update(double currentPos) {
+		if (reversed) currentPos = -currentPos;
+		
+		if (isFirstUpdate) {
+			// on the first call to update(), set up any initial state
+			// (which may be dependent on the initial position passed in)
+			lastUpdateTime = getTime();
+			pos = currentPos;
+			dt = vel = acc = 0.0;
+			output = doFirstUpdate();
+			isFirstUpdate = false;
+		} else if (done) {
+			return output; // we're done; just return the last output
+		} else {
+			// calculate delta time (dt) since the last update
+			double curTime = getTime();
+			dt = curTime - lastUpdateTime;
+			if (dt < dtMin) return output; // called too rapidly; wait to get a more accurate delta
+			lastUpdateTime = curTime;
+			
+			// compute the current velocity and acceleration
+			double lastVel = vel;
+			vel = (currentPos - pos) / dt;
+			pos = currentPos;
+			acc = (vel - lastVel) / dt;
+			
+			// update the output value
+			output = doUpdate();
+		}
+		
+		if (reversed) output = -output;
+		return output;
+	}
 	
 	public double getOutput() {
 		return output;
--- src/lib/control/TargetController.java
+++ src/lib/control/TargetController.java
@@ -12,13 +12,8 @@ public class TargetController extends Controller {
 	private double target;
 	private double stopPoint;
 	
-	private double pos, vel, acc;
-	private double lastUpdateTime;
-	
-	private boolean isFirstUpdate = true;
-	private boolean reversed;
-	
 	public TargetController(double target, double k_Speed, double k_MT, double k_Friction) {
+		super(0.001);
 		this.target = target;
 		this.k_Speed = k_Speed;
 		this.k_MT = k_MT;
@@ -26,57 +21,36 @@ public class TargetController extends Controller {
 	}
 	
 	@Override
-	public double update(double currentPos) {
-		// on the first call to update(), set up the initial state
-		// (this is dependent on the initial position passed in)
-		if (isFirstUpdate) {
-			reversed = currentPos > target;
-			if (reversed) {
-				// if the target is in the negative direction, invert
-				// the input & output so that internal inequalities
-				// and such still work as intended
-				target = -target;
-				currentPos = -currentPos;
-			}
-			stopPoint = (target+currentPos)/2;
-			lastUpdateTime = getTime();
-			
-			isFirstUpdate = false;
-			return output = 0.0;
-		} else if (done) {
-			return output; // we're done; just return the last output (0.0)
+	protected double doFirstUpdate() {
+		reversed = pos > target;
+		if (reversed) {
+			// if the target is in the negative direction, invert
+			// the input & output so that internal inequalities
+			// and such still work as intended
+			target = -target;
+			pos = -pos;
 		}
-		
-		// calculate delta time (dt) since the last update
-		double curTime = getTime();
-		double dt = curTime - lastUpdateTime;
-		if (dt < 0.001) return output; // called too rapidly; wait to get a more accurate delta
-		lastUpdateTime = curTime;
-		
-		// compute the current velocity and acceleration
-		if (reversed) currentPos = -currentPos;
-		double lastVel = vel;
-		vel = (currentPos - pos) / dt;
-		pos = currentPos;
-		acc = (vel - lastVel) / dt;
-		
+		stopPoint = (target+pos)/2;
+		return 0.0;
+	}
+	
+	@Override
+	protected double doUpdate() {
 		if (pos < stopPoint) {
 			// before we reach the stop point, go full speed ahead
-			return output = reversed? -1.0 : 1.0;
+			return 1.0;
 		} else if (pos < target) {
 			// after the stop point, control the output
 			double dist = target - pos;
-			output = vel*k_Speed - (vel*vel)*k_MT/(dist*2) - k_Friction*k_MT;
-			if (reversed) output = -output;
-			return output;
+			return vel*k_Speed - (vel*vel)*k_MT/(dist*2) - k_Friction*k_MT;
 		} else {
 			// once we're at/past the target, stop and finish
 			double stopTime = -vel/acc;
 			if (stopTime > 0.05) { // make sure we've come to a complete stop
-				return output = reversed? 0.5 : -0.5; // half speed backward
+				return -0.5; // half speed backward
 			} else {
 				done = true;
-				return output = 0.0;
+				return 0.0;
 			}
 		}
 	}
